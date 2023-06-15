12448ac57ec225a6050d056840d5e35b7edd28ca
Vankad24 <vankad24@mail.ru>
2023-06-15 15:37:27 +0800

    Experiment with async, explore FastAPI basics

--- telegram-bot/fast_api.py
+++ telegram-bot/fast_api.py
@@ -1,5 +1,8 @@
-import os
 from fastapi import FastAPI
+from fastapi.responses import HTMLResponse
+import uvicorn
+import asyncio
+import main_bot as bot
 
 # http://127.0.0.1:8000/openapi.json
 # http://127.0.0.1:8000/docs
@@ -10,9 +13,40 @@ app = FastAPI()
 
 @app.get("/")
 async def root():
-    return {"message": "Hello World"}
+    html_content = """
+    <html>
+    <head>
+        <title>My HTML Page</title>
+    </head>
+    <body>
+        <h1>Welcome to FastAPI!</h1>
+        <p>Useful links:</p>
+        <a href='/docs'>Документация</a><br>
+        <a href='/redoc'>Другая документация</a><br>
+        <a href='/openapi.json'>Json документация</a><br>
+    </body>
+    </html>
+    """
+    return HTMLResponse(content=html_content, status_code=200)
 
 
+
+@app.get("/test")
+async def read_item(num: int, q: str | None = None):
+    if q:
+        return {"num":num, "q": q}
+    return {"num":num}
+
+
+async def start_api():
+    print("FastAPI is running...")
+    uvicorn.run("fast_api:app", host="0.0.0.0", port=8000, reload=True)
+
+async def main():
+    # await asyncio.gather(start_api(), bot.start_bot())
+    await asyncio.gather(start_api())
 
 if __name__ == "__main__":
-    os.system("uvicorn fast_api:app --reload")
+    asyncio.run(main())
+
+# os.system("uvicorn fast_api:app --reload")
--- telegram-bot/main_bot.py
+++ telegram-bot/main_bot.py
@@ -2,82 +2,89 @@ import requests
 import telebot
 from telebot import types
 import secrets
+import asyncio
+from telebot.async_telebot import AsyncTeleBot
+
 
 subscribers = [5718232858]
 
-# from telebot.async_telebot import AsyncTeleBot
+bot = AsyncTeleBot(secrets.API_TOKEN)
 
-# bot = AsyncTeleBot(API_TOKEN)
-
-
-bot = telebot.TeleBot(secrets.API_TOKEN)
+# bot = telebot.TeleBot(secrets.API_TOKEN)
 
 
 @bot.message_handler(commands=['help', 'start'])
-def send_welcome(message):
-	bot.reply_to(message, """\
+async def send_welcome(message):
+	await bot.reply_to(message, """\
 Hi there, I am EchoBot.
 I am here to echo your kind words back to you. Just say anything nice and I'll say the exact same thing to you!\
 """)
 
 
 @bot.message_handler(commands=['id'])
-def command_id(message):
+async def command_id(message):
 	# print(message)
-	bot.send_message(message.chat.id, "Ваш id:\n"+str(message.chat.id))
+	await bot.send_message(message.chat.id, "Ваш id:\n"+str(message.chat.id))
 
 
 @bot.message_handler(commands=['notify_all'])
-def command_notify_all(message):
+async def command_notify_all(message):
 	text = message.text[len("/notify_all"):]
 	if text:
-		notify_all(text)
-		bot.send_message(message.chat.id, f"Сообщение отправлено {len(subscribers)} пользователям")
+		await notify_all(text)
+		await bot.send_message(message.chat.id, f"Сообщение отправлено {len(subscribers)} пользователям")
 	else:
-		bot.send_message(message.chat.id, "Вы не ввели сообщение для отправки")
+		await bot.send_message(message.chat.id, "Вы не ввели сообщение для отправки")
 
 
 
 @bot.message_handler(commands=['joke'])
-def send_joke(message):
+async def send_joke(message):
 	joke_text = requests.get('https://geek-jokes.sameerkumar.website/api?format=json').json()["joke"]
-	bot.send_message(message.chat.id, joke_text)
+	await bot.send_message(message.chat.id, joke_text)
 
 
-def notify_all(text):
+async def notify_all(text):
+	tasks = []
 	for uid in subscribers:
-		notify(uid, text)
+		tasks.append(notify(uid, text))
+	await asyncio.gather(*tasks)
 
 
-def notify(user_id, text):
+async def notify(user_id, text):
     keyboard = types.InlineKeyboardMarkup()
     link_button = types.InlineKeyboardButton("Сайт", url='https://simurg.iszf.irk.ru/')
     ok_button = types.InlineKeyboardButton("Ок", callback_data='ok')
     keyboard.add(link_button, ok_button)
     
-    bot.send_message(user_id, text, reply_markup=keyboard)
+    await bot.send_message(user_id, text, reply_markup=keyboard)
 
 
 # func - filter function. Call Obfect fields https://pytba.readthedocs.io/en/latest/types.html#telebot.types.CallbackQuery
 @bot.callback_query_handler(func=lambda call: True)
-def handle_button_click(call):
+async def handle_button_click(call):
+    msg = "Что-то пошло не так"
     # if call.data == 'repeat':
         # Отправляем сообщение повторно
         # bot.send_message(call.message.chat.id, "Привет!", reply_markup=call.message.reply_markup)
         # test(call.message)
     if call.data == 'ok':
-        # Отправляем сообщение "Всё ок!"
-        bot.send_message(call.message.chat.id, "Напоминание выключено")
+    	msg = "Напоминание выключено"
+    
+    await bot.send_message(call.message.chat.id, msg)
 
 
 # Handle all other messages with content_type 'text' (content_types defaults to ['text'])
 @bot.message_handler(func=lambda message: True)
-def echo_message(message):
-	bot.reply_to(message, f'You said: "{message.text}"!')
+async def echo_message(message):
+	await bot.reply_to(message, f'You said: "{message.text}"!')
 
 
 
+async def start_bot():
+	print("Bot is running...")
+	# notify_all("Какой-то сервис не работает")
+	await bot.polling()
 
-print("Bot is running...")
-notify_all("Какой-то сервис не работает")
-bot.infinity_polling()
+if __name__ == "__main__":
+    asyncio.run(start_bot())
