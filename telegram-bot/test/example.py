c4c3ef204b581505771bc6dea5ab363d451ec218
Vankad24 <vankad24@mail.ru>
2023-06-23 16:54:03 +0800

    Try to run the bot in multiple threads

--- telegram-bot/test/example.py
+++ telegram-bot/test/example.py
@@ -2,6 +2,7 @@ import secrets
 from telebot.async_telebot import AsyncTeleBot
 import asyncio
 import time
+import threading
 
 bot = AsyncTeleBot(secrets.API_TOKEN)
 
@@ -13,8 +14,8 @@ async def check_db():
         global count
         count += 1
         print(count)
-        # await asyncio.sleep(1)
-        time.sleep(1)
+        await asyncio.sleep(1)
+        # time.sleep(1)
 
 
 @bot.message_handler(commands=['count'])
@@ -23,10 +24,21 @@ async def send_welcome(message):
 
 
 async def run():
-    await asyncio.gather(check_db(), bot._process_polling())
+    await asyncio.gather(check_db(), bot.polling())
 
 
-if __name__ == "__main__":
+def start():
     asyncio.run(run())
 
-# запустить в 4 потока
+if __name__ == "__main__":
+    start()
+    
+    threads = []
+    # запустить в 4 потока
+    # for i in range(4):
+    #     t = threading.Thread(target=start)
+    #     threads.append(t)
+    #     t.start()
+
+    # for t in threads:
+    #     t.join()
