09919cb264cbec0a6e9b3b0426a82f37f92617c9
Vankad24 <vankad24@mail.ru>
2023-07-07 16:46:19 +0800

    Add start and joke commands

--- telegram-bot/src/command_handlers.py
+++ telegram-bot/src/command_handlers.py
@@ -2,20 +2,17 @@ from .settings import bot
 from . import messages as ms
 from telebot.types import Message
 from .db_functions import test_subscription
+import requests
 
 @bot.message_handler(commands=["start"])
 async def handle_start_command(message: Message) -> None:
-    # # Если пользователь новый - регистрируем его
-    # if not user_exist(message.chat.id):
-    #     create_user(message.chat.id, message.from_user.username)
-    #     bot.send_message(message.chat.id, ms.WELCOME_MESSAGE, reply_markup=menu_markup)
-    # else:
-    #     bot.send_message(
-    #         message.chat.id, ms.ALREADY_REGISTERED_MESSAGE, reply_markup=menu_markup
-    #     )
-    #     insert_step(ms.MENU_STEP, message.chat.id)
     await bot.send_message(message.chat.id, ms.WELCOME_MESSAGE)
 
 @bot.message_handler(commands=["subscribe"])
 async def subscribe_command(message: Message) -> None:
     test_subscription()
+
+@bot.message_handler(commands=["joke"])
+async def joke_command(message: Message) -> None:
+    joke_text = requests.get('https://geek-jokes.sameerkumar.website/api?format=json').json()["joke"]
+    await bot.send_message(message.chat.id, joke_text)
