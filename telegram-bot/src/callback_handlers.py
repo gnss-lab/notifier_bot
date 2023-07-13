b8b31ddd7a18ea02698c6ba14fbac7c3f868a5e4
Vankad24 <vankad24@mail.ru>
2023-07-13 15:04:58 +0800

    Fix return button for 'my_subscriptions' and 'subscribe' commands

--- telegram-bot/src/callback_handlers.py
+++ telegram-bot/src/callback_handlers.py
@@ -1,6 +1,6 @@
 from .settings import bot
 from .functions import cancel_notification
-from .command_handlers import show_subscription_card, show_subscriptions_list, show_my_subscriptions, show_not_my_subscriptions
+from .command_handlers import show_subscription_card, show_my_subscriptions, show_not_my_subscriptions
 from .db_functions import set_notification, get_one_users_subscription, subscribe, unsubscribe
 from . import messages as ms
 from telebot.types import CallbackQuery
@@ -16,6 +16,7 @@ async def sub_callback(call: CallbackQuery) -> None:
     data = call.data.split(";")
     need_subscribe = data[1]=="on"
     sub_id = int(data[2])
+    show_my_subs = data[3] == 'True'
     message_id = call.message.message_id
     sub = get_one_users_subscription(user_id, sub_id)
     if need_subscribe:
@@ -23,7 +24,7 @@ async def sub_callback(call: CallbackQuery) -> None:
             subscribe(sub_id, user_id)
     elif sub:
         unsubscribe(sub_id, user_id)
-    await show_subscription_card(user_id, sub_id, message_id)
+    await show_subscription_card(user_id, sub_id, message_id, show_my_subs)
 
 @bot.callback_query_handler(func=lambda call: call.data.startswith(ms.CALLBACK_CHANGE_NOTIF))
 async def notif_callback(call: CallbackQuery) -> None:
@@ -31,22 +32,26 @@ async def notif_callback(call: CallbackQuery) -> None:
     data = call.data.split(";")
     remind = data[1]=="on"
     sub_id = int(data[2])
+    show_my_subs = data[3] == 'True'
     message_id = call.message.message_id
     set_notification(remind,sub_id, user_id)
-    await show_subscription_card(user_id, sub_id, message_id)
+    await show_subscription_card(user_id, sub_id, message_id, show_my_subs)
 
 @bot.callback_query_handler(func=lambda call: call.data.startswith(ms.CALLBACK_SHOW_SUB_CARD))
 async def show_sub_card_callback(call: CallbackQuery) -> None:
     user_id = call.from_user.id
     message_id = call.message.message_id
-    sub_id = int(call.data.split(";")[1])
-    await show_subscription_card(user_id, sub_id, changeable_message_id=message_id)
+    data = call.data.split(";")
+    sub_id = int(data[1])
+    show_my_subs = data[2] == 'True'
+    await show_subscription_card(user_id, sub_id, message_id, show_my_subs)
 
 @bot.callback_query_handler(func=lambda call: call.data.startswith(ms.CALLBACK_SHOW_SUB_LIST))
 async def show_sub_list_callback(call: CallbackQuery) -> None:
     user_id = call.from_user.id
     message_id = call.message.message_id
-    show_my_subs = call.data.split(";")[1] == "1"
+    data = call.data.split(";")
+    show_my_subs = data[1] == 'True'
     if show_my_subs:
         await show_my_subscriptions(user_id, message_id)
     else:
--- telegram-bot/src/command_handlers.py
+++ telegram-bot/src/command_handlers.py
@@ -31,50 +31,52 @@ async def my_subscriptions_command(message: Message) -> None:
 async def subscribe_command(message: Message) -> None:
     await show_not_my_subscriptions(message.chat.id)
 
-async def show_subscriptions_list(sub_list:list, user_id, success_message, fail_message,changeable_message_id=None):
+async def show_subscriptions_list(sub_list:list, user_id, success_message, fail_message, is_my_subscriptions, changeable_message_id=None):
     if sub_list:
         keyboard = types.InlineKeyboardMarkup(row_width=1)
         for sub in sub_list:
-            button = types.InlineKeyboardButton(sub.name, callback_data=f"{ms.CALLBACK_SHOW_SUB_CARD};{sub.id}")
+            button = types.InlineKeyboardButton(sub.name, callback_data=f"{ms.CALLBACK_SHOW_SUB_CARD};{sub.id};{is_my_subscriptions}")
             keyboard.add(button)
         if changeable_message_id:
             await bot.edit_message_text(success_message, user_id, changeable_message_id, reply_markup=keyboard)
         else:
             await bot.send_message(user_id, success_message, reply_markup=keyboard)
     else:
-        await bot.send_message(user_id, fail_message)
+        if changeable_message_id:
+            await bot.edit_message_text(fail_message, user_id, changeable_message_id)
+        else:
+            await bot.send_message(user_id, fail_message)
 
 async def show_my_subscriptions(user_id, changeable_message_id=None):
     subs = get_subscriptions_for_user(user_id)
-    await show_subscriptions_list(subs, user_id, ms.YOUR_SUBSCRIPTIONS, ms.NO_SUBSCRIPTIONS, changeable_message_id)
+    await show_subscriptions_list(subs, user_id, ms.YOUR_SUBSCRIPTIONS, ms.NO_SUBSCRIPTIONS, True, changeable_message_id)
 
 async def show_not_my_subscriptions(user_id, changeable_message_id=None):
     subs = get_not_subscriptions_for_user(user_id)
-    await show_subscriptions_list(subs, user_id, ms.SUBSCRIPTIONS_YOU_NOT_SUBSCRIBED, ms.YOU_SUBSCRIBED_EVERYTHING, changeable_message_id)
+    await show_subscriptions_list(subs, user_id, ms.SUBSCRIPTIONS_YOU_NOT_SUBSCRIBED, ms.YOU_SUBSCRIBED_EVERYTHING, False, changeable_message_id)
 
-async def show_subscription_card(user_id, sub_id, changeable_message_id=None, callback_my_subs=True):
+async def show_subscription_card(user_id, sub_id, changeable_message_id, callback_my_subs):
     sub_info = get_subscription_by_id(sub_id)
     sub = get_one_users_subscription(user_id, sub_id)
     keyboard = types.InlineKeyboardMarkup(row_width=2)
     text = f"{sub_info.name}\n{sub_info.description}\n"
     if sub:
         if sub.remind:
-            notif_button = types.InlineKeyboardButton(ms.TURN_OFF_NOTIF, callback_data=f"{ms.CALLBACK_CHANGE_NOTIF};off;{sub_id}")
+            notif_button = types.InlineKeyboardButton(ms.TURN_OFF_NOTIF, callback_data=f"{ms.CALLBACK_CHANGE_NOTIF};off;{sub_id};{callback_my_subs}")
             notif_mark = ms.REMIND_ON
         else:
-            notif_button = types.InlineKeyboardButton(ms.TURN_ON_NOTIF, callback_data=f"{ms.CALLBACK_CHANGE_NOTIF};on;{sub_id}")
+            notif_button = types.InlineKeyboardButton(ms.TURN_ON_NOTIF, callback_data=f"{ms.CALLBACK_CHANGE_NOTIF};on;{sub_id};{callback_my_subs}")
             notif_mark = ms.REMIND_OFF
-        sub_button = types.InlineKeyboardButton(ms.UNSUBSCRIBE, callback_data=f"{ms.CALLBACK_CHANGE_SUB};off;{sub_id}")
+        sub_button = types.InlineKeyboardButton(ms.UNSUBSCRIBE, callback_data=f"{ms.CALLBACK_CHANGE_SUB};off;{sub_id};{callback_my_subs}")
         keyboard.add(sub_button, notif_button)
 
         text+=f"{ms.SUBSCRIPTION}:{ms.SUBSCRIBED}\n{ms.NOTIFICATION}:{notif_mark}"
     else:
         text+=f"{ms.SUBSCRIPTION}:{ms.UNSUBSCRIBED}"
-        sub_button = types.InlineKeyboardButton(ms.SUBSCRIBE, callback_data=f"{ms.CALLBACK_CHANGE_SUB};on;{sub_id}")
+        sub_button = types.InlineKeyboardButton(ms.SUBSCRIBE, callback_data=f"{ms.CALLBACK_CHANGE_SUB};on;{sub_id};{callback_my_subs}")
         keyboard.add(sub_button, row_width=2)
 
-    callback_data = ms.CALLBACK_SHOW_SUB_LIST+ ";" + ("1" if callback_my_subs else "0")
-    back_button = types.InlineKeyboardButton(ms.GO_BACK, callback_data=callback_data)
+    back_button = types.InlineKeyboardButton(ms.GO_BACK, callback_data=f"{ms.CALLBACK_SHOW_SUB_LIST};{callback_my_subs}")
     keyboard.add(back_button)
 
     if changeable_message_id:
