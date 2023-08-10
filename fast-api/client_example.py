7f26cd7459a7a3d5ea6e3e0ad1b947500a942456
Vankad24 <vankad24@mail.ru>
2023-08-10 16:41:06 +0800

    Update client example for multiple requests

--- fast-api/client_example.py
+++ fast-api/client_example.py
@@ -1,3 +1,5 @@
+import time
+
 import requests
 
 url = "http://127.0.0.1:8000"
@@ -7,7 +9,12 @@ r = session.post(f"{url}/login", data={"username": "user", "password": "123"})
 js = r.json()
 session.headers.update({"Authorization": f"Bearer {js['access_token']}", "Accept": "application/json"})
 
-r = session.get(f"{url}/send_notification_by_id", params={"message": "Test message", "sub_id": 3})
+def send_notif(message):
+    r = session.get(f"{url}/send_notification_by_id", params={"message": message, "sub_id": 1})
+    print(r.status_code)
+    print(r.text)
 
-print(r.status_code)
-print(r.text)
+for j in range(10):
+    for i in range(10):
+        send_notif(f"Test message {i}")
+    time.sleep(1)
